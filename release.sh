938cb1ab3b051cdc46c8f39d944feefe6f477ffb
Arthur <site-github@api-d.com>
2022-03-21 13:06:35 +0100

    Update release script for packaging

--- release.sh
+++ release.sh
@@ -13,21 +13,22 @@ PREVIOUS_GIT_TAG=$(git describe --abbrev=0 --tags)
 echo -e "==> Previous version was v$COLOR_RED$PREVIOUS_GIT_TAG$COLOR_NC"
 
 # Ensure working tree is clean or ask to commit
-if [[ -n $(git status --porcelain) ]]; then
+# Don't continue while working tree isn't clean
+
+while true; do
+  if [[ -n $(git status --porcelain) ]]; then
     echo "==> ! Working tree is not clean. Please commit or stash changes before releasing. Press enter when done."
     # Wait for key press
     read -r
-fi
+  else
+    break
+  fi
+done
 
 # Ask for input
 echo "==> ! Enter the new version number (e.g. 1.2.3)> "
 read -r NEW_GIT_TAG
 
-# Run build script
-echo "==> Running build script"
-./build.sh
-
-twine check dist/*
 # Create tag for new release
 git tag "$NEW_GIT_TAG"
 echo "==> Created tag $NEW_GIT_TAG"
@@ -35,5 +36,10 @@ echo "==> Created tag $NEW_GIT_TAG"
 # Push tag to github
 git push origin "$NEW_GIT_TAG"
 echo "==> Pushed tag $NEW_GIT_TAG"
+
+# Run build script
+echo "==> Running build script"
+./build.sh
+twine check dist/*
 
 twine upload dist/*
